f784eca8df93c2ebcbbb959146beb40aea4941b4
lsbp <2803234009@qq.com>
2025-11-19 17:02:42 +0800

    添加日志记录

--- src/backend/app/core/config.py
+++ src/backend/app/core/config.py
@@ -3,6 +3,7 @@ from functools import lru_cache
 
 from app.config.base import BaseConfig
 from app.utils.profile import Profile
+from app.utils.log import log
 
 @lru_cache()
 def get_config(config_file="config.yaml", env=None) -> BaseConfig:
@@ -19,13 +20,16 @@ def get_config(config_file="config.yaml", env=None) -> BaseConfig:
     config_path = project_root.joinpath(config_file)
     # 读取 yaml 配置文件
     with open(config_path, "r", encoding="utf-8") as f:
+        log.info(f"Load config from {config_path}")
         yaml_config = yaml.safe_load(f)
 
     # 获取环境是dev还是prod
     if env:
         yaml_config["env"] = env
+        log.info(f"Use env {env}")
     else:
         env = yaml_config.get("env", "dev")
+        log.info(f"Use env {env}")
 
     # 根据环境获取对应的配置
     env_config = yaml_config.get(env, {})
--- src/backend/app/server.py
+++ src/backend/app/server.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from app.core.config import get_config
 from app.core.database import PsqlHelper
+from app.utils.log import log
 
 config = get_config()
 
@@ -17,6 +18,7 @@ async def startup_services(app: FastAPI):
     # 初始化config
     app.state.config = config
     # 初始化数据库连接
+    log.info("initialize database linking")
     app.state.psql_engine = await PsqlHelper.init_conn_psql(app.state.config.db)
 
 async def close_services(app: FastAPI):
@@ -27,6 +29,7 @@ async def close_services(app: FastAPI):
     """
 
     # 关闭数据库连接
+    log.info("close database linking")
     await PsqlHelper.close_conn_psql(app.state.psql_engine)
 
 @asynccontextmanager
@@ -38,7 +41,9 @@ async def lifespan(app: FastAPI):
     """
 
     await startup_services(app)
+    log.info("app startup")
     yield
+    log.info("app shutdown")
     await close_services(app)
 
 app = FastAPI(
