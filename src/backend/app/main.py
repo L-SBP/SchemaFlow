fe09bee69e1644d37d30f4a4f53ebffa317e1c1d
lsbp <2803234009@qq.com>
2025-11-19 16:51:53 +0800

    启动uvicorn

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,3 +1,5 @@
-from fastapi import FastAPI
+import uvicorn
+from server import config
 
-app = FastAPI()
+if __name__ == "__main__":
+    uvicorn.run(config.app.uvicorn, host=config.app.host, port=config.app.port, reload=config.app.reload)
