dd986d4a3fc744f4ec6a08ac0f0a1b87568c845e
lsbp <2803234009@qq.com>
2025-11-24 14:57:13 +0800

    登录，注册schema模型

--- src/backend/app/schema/auth.py
+++ src/backend/app/schema/auth.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel, Field, validator
 
+
 # 发送验证码
 class UserSendCode(BaseModel):
-    email: str = Field(..., min_length=3, max_length=50, discriminator="注册邮箱地址")
+    email: str = Field(..., min_length=3, max_length=50)
 
 # 用户注册
 class UserRegister(BaseModel):
-    username: str = Field(..., min_length=3, max_length=50, discriminator="用户名")
-    email: str = Field(..., min_length=3, max_length=50, discriminator="邮箱")
-    password: str = Field(..., min_length=6, max_length=50, discriminator="密码")
-    confirm_password: str = Field(..., min_length=6, max_length=50, discriminator="确认密码")
-    code:  str = Field(..., min_length=6, max_length=6, discriminator="验证码")
+    username: str = Field(..., min_length=3, max_length=50)
+    email: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(..., min_length=6, max_length=50)
+    confirm_password: str = Field(..., min_length=6, max_length=50)
+    verification_code:  str = Field(..., min_length=6, max_length=6)
 
     @validator('confirm_password')
     def password_match(cls, v, values):
@@ -20,15 +21,5 @@ class UserRegister(BaseModel):
 
 # 用户登录
 class UserLogin(BaseModel):
-    username: str = Field(..., min_length=3, max_length=50, discriminator="用户名或邮箱")
-    password: str = Field(..., min_length=6, max_length=50, discriminator="密码")
-
-# 自动登录
-class Token(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
-
-# 登录响应
-class TokenPayload(BaseModel):
-    sub: str = Field(..., discriminator="用户ID")
-    exp: int
+    username: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(..., min_length=6, max_length=50)
